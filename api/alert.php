d6b910f94075310b6c95ee9701e842ab614bd405
Sasha <40319171+lonagi@users.noreply.github.com>
2020-10-03 13:58:49 +0300

    Radius

--- api/alert.php
+++ api/alert.php
@@ -4,8 +4,16 @@ header('Content-Type: application/json');
 include_once "db.php";
 
 function api() {
-	if(isset($_GET['la1']) and isset($_GET['la2']) and isset($_GET['lo1']) and isset($_GET['lo2'])) {
-		$input = $_GET;
+	if(isset($_GET['la']) and isset($_GET['lo'])) {
+		$radius = 111; //km
+		$radian = $radius/111;
+		$input = [
+			'la1'=>$_GET['la']-$radian,
+			'la2'=>$_GET['la']+$radian,
+			'lo1'=>$_GET['lo']-$radian,
+			'lo2'=>$_GET['lo']+$radian
+		];
+
 		$db = R::find("redalert",'la1 >= ? and la2 <= ? and lo1 >= ? and lo2 <= ?',[$input['la1'],$input['la2'],$input['lo1'],$input['lo2']]);
 		if($db) {
 			$output=["Score"=>0];
