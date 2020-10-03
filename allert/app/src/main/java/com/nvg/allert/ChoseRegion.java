632267fae6365a925ca515e8f9d33b1c4dfb4584
Leonid <scorpion23997@gmail.com>
2020-10-03 19:10:59 +0300

    update spiner
    update spiner of country and region without design

--- allert/app/src/main/java/com/nvg/allert/ChoseRegion.java
+++ allert/app/src/main/java/com/nvg/allert/ChoseRegion.java
@@ -28,6 +28,9 @@ public class ChoseRegion extends Activity{
 //                Toast.makeText(getApplicationContext(),
 //                        "Запись - " + adapterView.getAdapter().getItem(pos).toString() + "\nПозиция - " + pos,
 //                        Toast.LENGTH_SHORT).show();
+              if(adapterView.getAdapter().getItem(pos).toString().trim().equals("Moldova")) setRegionMDSpiner();
+                if(adapterView.getAdapter().getItem(pos).toString().trim().equals("Ukraine")) setRegionUKSpiner();
+                if(adapterView.getAdapter().getItem(pos).toString().trim().equals("Romania")) setRegionROSpiner();
             }
             public void onNothingSelected(AdapterView<?> parent) {
                 Toast.makeText(getApplicationContext(),
@@ -39,9 +42,32 @@ public class ChoseRegion extends Activity{
 
     }
 
-    public void setRegionSpiner()
+    public void setRegionMDSpiner()
     {
-        
+        Spinner spinner = findViewById(R.id.regionSpiner);
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(this, R.array.RegOfMd,
+                        android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+    public void setRegionUKSpiner()
+    {
+        Spinner spinner = findViewById(R.id.regionSpiner);
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(this, R.array.RegOfUk,
+                        android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+    public void setRegionROSpiner()
+    {
+        Spinner spinner = findViewById(R.id.regionSpiner);
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(this, R.array.RegOfRo,
+                        android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
     void init()
     {
