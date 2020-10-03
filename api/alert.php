bc407d122ae40faed27e06f1b6d25ef37603a365
Sasha <40319171+lonagi@users.noreply.github.com>
2020-10-03 13:22:22 +0300

    Only One Marker for input

--- api/alert.php
+++ api/alert.php
@@ -4,9 +4,9 @@ header('Content-Type: application/json');
 include_once "db.php";
 
 function api() {
-	if(isset($_GET['la1']) and isset($_GET['la2']) and isset($_GET['lo1']) and isset($_GET['lo2'])) {
+	if(isset($_GET['la']) and isset($_GET['lo'])) {
 		$input = $_GET;
-		$db = R::find("redalert",'la1 >= ? and la2 <= ? and lo1 >= ? and lo2 <= ?',[$input['la1'],$input['la2'],$input['lo1'],$input['lo2']]);
+		$db = R::find("redalert",'la >= ? and lo <= ?',[$input['la'],$input['lo']]);
 		if($db) {
 			$output=["Score"=>0];
 
