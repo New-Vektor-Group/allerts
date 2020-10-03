66cd243911c7a03fa1503d8f4c22290a32e91f4c
Sasha <40319171+lonagi@users.noreply.github.com>
2020-10-03 10:30:12 +0300

    Update alert.php

--- api/alert.php
+++ api/alert.php
@@ -8,7 +8,21 @@ function api() {
 		$input = $_GET;
 		$db = R::find("redalert",'la1 >= ? and la2 <= ? and lo1 >= ? and lo2 <= ?',[$input['la1'],$input['la2'],$input['lo1'],$input['lo2']]);
 		if($db) {
-			
+			$output=["Score"=>0];
+
+			foreach ($db as $d) {
+				$output["Score"]+=1;
+				
+				$data = [
+					"la1"=>floatval($d->la1),
+					"la2"=>floatval($d->la2),
+					"lo1"=>floatval($d->lo1),
+					"lo2"=>floatval($d->lo2)
+				];
+
+				$output["dot".$output["Score"]]=$data;
+			}
+			return $output;
 		}
 		else
 			return false;
